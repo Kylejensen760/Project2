34077ca0eb580e16b4af7624ded1842b0fd8f883
Kyle Jensen <kylejensen760@gmail.com>
2019-04-02 22:47:31 -0400

    Added more beans

--- Frugal/src/main/java/com/titans/beans/Menu.java
+++ Frugal/src/main/java/com/titans/beans/Menu.java
@@ -1,5 +1,28 @@
 package com.titans.beans;
 
-public class Menu {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "menu")
+public class Menu {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@Column(name = "restaurant_id")
+	private Long restaurantId;
+	
+	@Column(name = "time_begin")
+	private Long begginingTime;
+	
+	@Column(name = "time_end")
+	private Long endTime;
 }
