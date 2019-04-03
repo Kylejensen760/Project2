5ad3966aa48f457a188ec7bb31f2c4d815a7029e
Kyle Jensen <kylejensen760@gmail.com>
2019-04-03 12:55:00 -0400

    Testing Jenkins

--- Frugal/src/main/java/com/titans/beans/Review.java
+++ Frugal/src/main/java/com/titans/beans/Review.java
@@ -7,8 +7,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="review")
 public class Review implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
