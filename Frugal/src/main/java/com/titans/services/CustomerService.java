ca8c6711091abd9a3be40be4ba402297027fa16b
Kyle Jensen <kylejensen760@gmail.com>
2019-04-05 19:28:20 -0400

    updated project

--- Frugal/src/main/java/com/titans/services/CustomerService.java
+++ Frugal/src/main/java/com/titans/services/CustomerService.java
@@ -1,7 +1,14 @@
 package com.titans.services;
 
-import com.titans.beans.Customer;
+import java.util.List;
 
+import com.titans.beans.Customer;
+ 
 public interface CustomerService {
-	public Customer login(String user, String pass);
+	public Customer login(String username, String password);
+	public Customer getCustomerById(int i);
+	public List<Customer> getCustomers();
+	public void deleteCustomer(Customer cust);
+	public void updateCustomer(Customer cust);
+	public void newCustomer(Customer cust);
 }
