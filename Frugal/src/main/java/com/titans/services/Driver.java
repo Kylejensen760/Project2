07900dcfdc754cf5bc7b12409e307e69cce680f7
Kyle Jensen <kylejensen760@gmail.com>
2019-04-04 21:38:43 -0400

    added SQL

--- Frugal/src/main/java/com/titans/services/Driver.java
+++ Frugal/src/main/java/com/titans/services/Driver.java
@@ -4,15 +4,14 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.titans.data.CustomerDao;
+import com.titans.data.CustomerHibernate;
 
 public class Driver {
 	private static ApplicationContext ac;
 	public static void main(String[] args) {
-		ac = new ClassPathXmlApplicationContext("beans.xml");
 		
-		CustomerDao cd = (CustomerDao) ac.getBean(CustomerDao.class);
+		CustomerDao cd = new CustomerHibernate();
 		System.out.println(cd.getAllCustomers());
-		System.exit(0);
 		
 	}
 }
