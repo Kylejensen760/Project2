d18bc4f87471f1683d734fe2c2df67cefc79f017
Kyle Jensen <kylejensen760@gmail.com>
2019-04-04 20:32:13 -0400

    Added xml back

--- Frugal/src/main/java/com/titans/controllers/LoginController.java
+++ Frugal/src/main/java/com/titans/controllers/LoginController.java
@@ -4,25 +4,25 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.titans.beans.Customer;
 import com.titans.services.CustomerService;
 
 @Controller
-//@RequestMapping(value="/login")
+@RequestMapping(value="/login")
 public class LoginController {
 	@Autowired
 	private CustomerService cs;
 	
-//	@RequestMapping(method=RequestMethod.GET)
+	@RequestMapping(method=RequestMethod.GET)
 	public String goLogin(HttpSession session) {
 		if(session.getAttribute("user")!=null)
 			return "redirect:home";
 		return "static/login.html";
 	}
-//	@RequestMapping(method=RequestMethod.POST)
+	@RequestMapping(method=RequestMethod.POST)
 	public String login(HttpSession session, String username,  String password) {
 		Customer c = cs.login(username, password);
 		System.out.println(username);
