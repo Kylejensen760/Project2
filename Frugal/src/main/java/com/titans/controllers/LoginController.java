a693fe774889f02112b8c342bff74332fabffa0a
cesarFrangelico <cesar.frangelico@gmail.com>
2019-04-06 20:47:21 -0400

    more changes

--- Frugal/src/main/java/com/titans/controllers/LoginController.java
+++ Frugal/src/main/java/com/titans/controllers/LoginController.java
@@ -1,48 +1,48 @@
-
 package com.titans.controllers;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.titans.beans.Customer;
-import com.titans.beans.Restaurant;
 import com.titans.beans.LoginInfo;
 import com.titans.services.CustomerService;
-//import com.titans.services.RestaurantService;
 
-@Controller
+@RestController
 @RequestMapping(value="/login")
+@CrossOrigin(origins="http://localhost:4200")
 public class LoginController {
 
-	@Autowired
-	private CustomerService cs;
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public String goLogin(HttpSession session) {
-		if(session.getAttribute("user")!=null)
-			return "redirect:home";
-		return "static/login.html";
-	}
-	
-	@RequestMapping(method=RequestMethod.POST)
-	public String login(HttpSession session, String username,  String password) {
-		Customer c = cs.login(username, password);
-		System.out.println("============== login list " + c);
-		if(c != null) {
-			session.setAttribute("user", c);
-
-			return "redirect:home";
-		}
-		return "redirect:login";
-	}
-	
-	@RequestMapping(method=RequestMethod.DELETE)
-	public void logout(HttpSession session) {
-		session.invalidate();
-	}
+    @Autowired
+    private CustomerService cs;
+    
+    @RequestMapping(method=RequestMethod.GET)
+    public LoginInfo login(HttpSession session) {
+    	System.out.println("here this is a get");
+    	
+        return (LoginInfo) session.getAttribute("loggedUser");
+    }
+    
+    @RequestMapping(method=RequestMethod.POST)
+    public LoginInfo login(@RequestParam("user") String username, 
+            @RequestParam("pass") String password, HttpSession session) {
+        Customer c = cs.login(username,  password);
+        System.out.println("here");
+        if(c==null) {
+            return null;
+        }
+        LoginInfo loggedUser = new LoginInfo(c);
+        session.setAttribute("loggedUser", loggedUser);
+        return loggedUser;
+    }
+    
+    @RequestMapping(method=RequestMethod.DELETE)
+    public void logout(HttpSession session) {
+        session.invalidate();
+    }
 }
