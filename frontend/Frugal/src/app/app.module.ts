7c1ed8eadc20282465249ec1e42c7d8dd66b63eb
Karl Cortez <karlcortez727@gmail.com>
2019-04-10 22:49:28 -0400

    before merging

--- frontend/Frugal/src/app/app.module.ts
+++ frontend/Frugal/src/app/app.module.ts
@@ -1,42 +1,4 @@
-<<<<<<< HEAD
-import { BrowserModule } from '@angular/platform-browser';
 
-import { NgModule} from '@angular/core';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { UrlService } from './shared/url.service';
-import { UserService } from './shared/user/user.service';
-import { LoginComponent } from './core/login/login.component';
-import { CreateComponent } from './create/create.component';
-import { AccountCreationComponent } from './shared/account-creation/account-creation.component';
-
-@NgModule({
-  declarations: [
-    AppComponent, 
-    NavbarComponent,
-    LoginComponent,
-    CreateComponent,
-    AccountCreationComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    MDBBootstrapModule.forRoot(),
-    FormsModule,
-    HttpClientModule
-
-  ],
-
-  providers: [UrlService, UserService],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
-=======
 import { BrowserModule } from '@angular/platform-browser';
 
 import { NgModule} from '@angular/core';
@@ -69,4 +31,4 @@ import { UserService } from './shared/user/user.service';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
->>>>>>> 9ade7b050fd6b18dd916aae08c76b8617713fa8d
+
