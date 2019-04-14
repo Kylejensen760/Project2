7f021ff9d3e32c65819be0125b64fd72c4cc38da
Karl Cortez <karlcortez727@gmail.com>
2019-04-14 17:33:26 -0400

    made component and service to add reviews in angular

--- frontend/Frugal/src/app/app.module.ts
+++ frontend/Frugal/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MenuComponent } from './menu/menu.component';
 import { CarouselComponent } from './core/carousel/carousel.component';
 import { RestaurantViewComponent } from './restaurantView/restaurant-view/restaurant-view.component';
 import { AccountCreationComponent } from './shared/account-creation/account-creation.component';
+import { ReviewComponent } from './shared/review/review.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { AccountCreationComponent } from './shared/account-creation/account-crea
     MenuComponent,
     CarouselComponent,
     RestaurantViewComponent,
-    AccountCreationComponent
+    AccountCreationComponent,
+    ReviewComponent
   ],
   imports: [
     BrowserModule,
